7480cc158322b1929d8a90095ac2fc776a253c66
Shredder <mashefor@gmail.com>
2019-09-19 06:03:46 +0100

    update node modules and backend service

--- src/app/fakebackend.service.ts
+++ src/app/fakebackend.service.ts
@@ -8,81 +8,81 @@ export class FakeBackendService implements InMemoryDbService {
                 id: 1,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j.png',
-                price: faker.commerce.price()
+                img: '../assets/j1.jpg',
+                price: faker.commerce.price(2)
             },
             {   id: 2,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j2.png',
-                price: faker.commerce.price()
+                img: '../assets/j2.jpg',
+                price: faker.commerce.price(2)
             },
             {   id: 3,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j4.png',
-                price: faker.commerce.price()
+                img: '../assets/j3.jpg',
+                price: faker.commerce.price(2)
             },
             {
                 id: 4,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j3.png',
-                price: faker.commerce.price()
+                img: '../assets/j4.jpg',
+                price: faker.commerce.price(2)
             },
             {   id: 5,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j7.png',
-                price: faker.commerce.price()
+                img: '../assets/j5.jpg',
+                price: faker.commerce.price(2)
             },
             {   id: 6,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j.png',
-                price: faker.commerce.price()
+                img: '../assets/j6.jpg',
+                price: faker.commerce.price(2)
             },
             {
                 id: 7,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j.png',
-                price: faker.commerce.price()
+                img: '../assets/j7.jpg',
+                price: faker.commerce.price(2)
             },
             {
                 id: 8,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j3.png',
-                price: faker.commerce.price()
+                img: '../assets/j8.jpg',
+                price: faker.commerce.price(2)
             },
             {
                 id: 9,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j8.png',
-                price: faker.commerce.price()
+                img: '../assets/j9.jpg',
+                price: faker.commerce.price(2)
             },
             {
                 id: 10,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j7.png',
-                price: faker.commerce.price()
+                img: '../assets/j1.jpg',
+                price: faker.commerce.price(2)
             },
             {
                 id: 11,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j7.png',
+                img: '../assets/j2.jpg',
                 price: faker.commerce.price()
             },
             {
                 id: 12,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j.png',
-                price: faker.commerce.price()
+                img: '../assets/j3.jpg',
+                price: faker.commerce.price(2)
             },
         ]
         let cart = [
@@ -90,8 +90,8 @@ export class FakeBackendService implements InMemoryDbService {
                 id: 12,
                 product_name: "Product Name",
                 description: faker.lorem.paragraphs(),
-                img: '../assets/j.png',
-                price: faker.commerce.price()
+                img: '../assets/j.jpg',
+                price: faker.commerce.price(2)
             },
         ]
         return { collection, cart }
